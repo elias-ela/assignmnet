0f45d0b9e42c088ca3ea91d0f15284de1e8d7c65
elias-ela <eliasfeleke5@gmail.com>
2025-02-19 13:30:21 +0400

    add env and report configuration

---
+++ question1/cypress.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require("cypress");
+require("dotenv").config();
+
+module.exports = defineConfig({
+  env: {
+    firstname: process.env.FIRSTNAME,
+    lastname: process.env.LASTNAME,
+    email: process.env.EMAIL,
+    password: process.env.PASSWORD,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.baseUrl = process.env.BASE_URL
+
+      return config;
+    },
+    testIsolation: false,
+    
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "reports",
+      reportFilename: "index",
+      overwrite: true,
+      html: true,
+      json: true,
+    },
+  },
+});
